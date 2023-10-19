a0e9ec5db7c184ec5cddf6f990bc8807aa7ccf51
Kasonde Besa <61287639+KayBesa@users.noreply.github.com>
2023-10-19 17:59:44 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+/**
+    Name: Kasonde Besa
+    Script: app.js
+    Description: Application that uses the game logic for simple WebApp of Tic-Tac-Toe
+    Date: October 19th, 2023
+**/
+
+// Get dynamic components of the container
+
+let userText = document.getElementById("userText");
+let restartBtn = document.getElementById("restartButton");
+let gameCells = Array.from(document.getElementsByClassName("cell"));
