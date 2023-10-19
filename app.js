d82a4c4071204938d053e4606d64034ffea71a2a
Kasonde Besa <61287639+KayBesa@users.noreply.github.com>
2023-10-19 18:24:47 -0400

    created logic to fill in cells upon click

--- app.js
+++ app.js
@@ -10,3 +10,28 @@
 let userText = document.getElementById("userText");
 let restartBtn = document.getElementById("restartButton");
 let gameCells = Array.from(document.getElementsByClassName("cell"));
+
+// Variables used to fill cells and keep track of filled cells.
+const LETTER_O = "O";
+const LETTER_X = "X";
+let currPlayer = LETTER_X; // Game starts with X
+
+let filledCell = Array(9).fill(null);
+
+/* Functions that add functionality to the gameboard. */
+
+// Starts game by making gameboard clickable.
+const startGame = () => {
+    gameCells.forEach(cell => cell.addEventListener('click', clickedACell));
+}
+
+// Fills cell with letter 'X' or 'O', doesn't change already filled cell.
+function clickedACell(event) {
+    const id = event.target.id;
+    if (!filledCell[id]) {
+        filledCell[id] = currPlayer;
+        event.target.innerText = currPlayer;
+    }
+}
+
+startGame();
